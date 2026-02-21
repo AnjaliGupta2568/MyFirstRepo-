878a11ca42cd3e29937c3a7f27cbbaebf4d0d44c
Anjali Gupta <anjalgupta100@gmail.com>
2026-02-21 21:25:13 +0530

    Add files via upload

---
+++ Advance SQL/2-Select Statement.sql
@@ -0,0 +1,31 @@
+-- SELECT STATEMENET
+
+SELECT * 
+FROM parks_and_recreation.employee_demographics;
+
+SELECT last_name, first_name, gender, age
+FROM employee_demographics;
+
+SELECT last_name, 
+first_name, 
+gender, 
+age
+FROM employee_demographics;
+
+SELECT first_name, 
+last_name,
+salary,
+salary + 100
+FROM employee_salary;
+
+SELECT first_name, 
+last_name,
+salary,
+(salary + 100) * 10
+FROM employee_salary;
+
+SELECT dept_id
+FROM employee_salary;
+
+SELECT DISTINCT dept_id
+FROM employee_salary;
